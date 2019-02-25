be1f176fd3c65a4476ff79ce98d13ffd7acdebd8
Angela <Angela@10.0.0.34>
2019-02-24 22:34:50 -0500

    First Commit

---
+++ FairyTale/src/main/java/fairytale/com/angelaquillen/FairyTale/FairyTaleApplication.java
@@ -0,0 +1,13 @@
+package fairytale.com.angelaquillen.FairyTale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairyTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairyTaleApplication.class, args);
+	}
+
+}
---
+++ FairyTale/src/test/java/fairytale/com/angelaquillen/FairyTale/FairyTaleApplicationTests.java
@@ -0,0 +1,16 @@
+package fairytale.com.angelaquillen.FairyTale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FairyTaleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

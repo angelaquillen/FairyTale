afa4c69e5b97008c653c9bc712977f6642d06aae
Angela <Angela@10.0.0.34>
2019-02-25 00:20:29 -0500

    update files

--- FairyTale/src/main/java/fairytale/com/angelaquillen/FairyTale/FairyTaleApplication.java
+++ FairyTale/src/main/java/fairytale/com/angelaquillen/FairyTale/FairyTaleApplication.java
@@ -2,10 +2,17 @@ package fairytale.com.angelaquillen.FairyTale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class FairyTaleApplication {
 
+	@RequestMapping("/index")
+	@ResponseBody
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FairyTaleApplication.class, args);
 	}
